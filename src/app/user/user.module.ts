63c1931ac2c49e8a42b0943902dbf0765cd9f569
Paulo-Teixeira <paulo.jtfs@gmail.com>
2019-10-14 16:07:58 +0200

    Adds login page with Google sign-in functionalities

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { UserRoutingModule } from './user-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { GoogleSigninDirective } from './google-signin.directive';
 
 @NgModule({
-  declarations: [LoginPageComponent],
+  declarations: [LoginPageComponent, GoogleSigninDirective],
   imports: [UserRoutingModule, SharedModule]
 })
 export class UserModule {}
