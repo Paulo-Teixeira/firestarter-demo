49b7115897719baf825f914ed41db96f3aad8078
Paulo-Teixeira <paulo.jtfs@gmail.com>
2019-10-14 16:45:27 +0200

    Imports reactive forms module in user module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,9 +4,10 @@ import { UserRoutingModule } from './user-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { GoogleSigninDirective } from './google-signin.directive';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginPageComponent, GoogleSigninDirective],
-  imports: [UserRoutingModule, SharedModule]
+  imports: [UserRoutingModule, SharedModule, ReactiveFormsModule]
 })
 export class UserModule {}
